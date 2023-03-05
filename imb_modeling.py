e1d9ae7ee9cdc6a090f83a5ee466ddefb1eca016
Nicole Hye Won Hwang <hyewonee96@gmail.com>
2023-03-05 09:26:32 -0600

    added joblib to dump model

--- imb_modeling.py
+++ imb_modeling.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 #imblearn
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
+import joblib
 
 def imb_pipe_fit(model, params, X, y, score='roc_auc', scaler=False):
     """
@@ -36,8 +37,8 @@ def imb_pipe_fit(model, params, X, y, score='roc_auc', scaler=False):
                                      ['model', model]]
                             )
         
-    folds = RepeatedStratifiedKFold(n_splits=10, 
-                                    n_repeats=3,
+    folds = RepeatedStratifiedKFold(n_splits=2, 
+                                    n_repeats=1,
                                     random_state=421
                                     )   
     
@@ -53,4 +54,6 @@ def imb_pipe_fit(model, params, X, y, score='roc_auc', scaler=False):
     gs.fit(X_train, y_train)
     cv_score = gs.best_score_
     test_score = gs.score(X_test, y_test)
+    joblib.dump(gs, f"{type(model).__name__}.pkl")
+
     return {'model':gs, 'cv_score':cv_score, 'test_score':test_score, 'best_param':gs.best_params_, "cv_results":gs.cv_results_}
